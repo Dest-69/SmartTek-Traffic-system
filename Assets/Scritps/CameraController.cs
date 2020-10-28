16854730b9af7baec3a86033a587adc620ae4c7d
Dest_69 <vetalpopovetskiy@gmail.com>
2020-10-29 00:19:10 +0200

    some stuff

---
+++ Assets/Scritps/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Image RecordImage;
+    [SerializeField] private GameObject CameraRotateObj;
+    [SerializeField] private float CameraRotateField;
+    [SerializeField] private float CameraSpeedRotation;
+    void Start()
+    {
+        StartCoroutine(REC());
+    }
+    void FixedUpdate()
+    {
+        if (CameraRotateObj.transform.rotation.y > CameraRotateField || CameraRotateObj.transform.rotation.y < -CameraRotateField) CameraSpeedRotation *= -1;
+        CameraRotateObj.transform.Rotate(new Vector3(0, CameraRotateField, 0), CameraSpeedRotation);
+    }
+
+    IEnumerator REC()
+    {
+        while (true)
+        {
+            RecordImage.enabled = true;
+            yield return new WaitForSeconds(2f);
+            RecordImage.enabled = false;
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
