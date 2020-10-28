1036c2e3b4e7abf888222113503b2cfb833b47c7
Dest_69 <vetalpopovetskiy@gmail.com>
2020-10-28 20:48:19 +0200

    + do not lo0k at me

--- Assets/Scritps/CarMove.cs
+++ Assets/Scritps/CarMove.cs
@@ -4,15 +4,137 @@ using UnityEngine;
 
 public class CarMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private RaycastHit hit = new RaycastHit();
+    [SerializeField] private float Distance;
+    [SerializeField] private float SpeedMove;
+    enum vectorMove
     {
-        
+        Up,
+        Down,
+        Left,
+        Right
+    }
+    [SerializeField] private vectorMove VectorMove;
+    void FixedUpdate()
+    {
+        if (Physics.Raycast(transform.position, transform.right, out hit, Mathf.Infinity))
+        {
+            switch (hit.collider.gameObject.tag)
+            {
+                case "bike":
+                    if (hit.distance > Distance)
+                        Move();
+                    return;
+
+                case "stop":
+                    if (hit.distance > Distance)
+                        Move();
+                    return;
+
+                case "CrossroadsRight":
+
+                    if (hit.distance < 0.05f)
+                    {
+                        bool isBikeTurn;
+                        isBikeTurn = (Random.value < 0.5);
+                        if (isBikeTurn)
+                        {
+                            Turn(90);
+                            TurnBike("right");
+                            StartCoroutine(HideForRayCast(hit.collider));
+                        }
+                        else Move();
+                    }
+                    Move();
+                    return;
+                case "CrossroadsLeft":
+
+                    if (hit.distance < 0.05f)
+                    {
+                        bool isBikeTurn;
+                        isBikeTurn = (Random.value < 0.5);
+                        if (isBikeTurn)
+                        {
+                            Turn(-90);
+                            TurnBike("left");
+                            StartCoroutine(HideForRayCast(hit.collider));
+                        }
+                        else Move();
+                    }
+                    Move();
+                    return;
+
+                case "turnRight":
+                    if (hit.distance < 0.05f)
+                    {
+                        Turn(90);
+                        TurnBike("right");
+                        StartCoroutine(HideForRayCast(hit.collider));
+                    }
+                    else Move();
+                    return;
+
+                case "turnLeft":
+                    if (hit.distance < 0.05f)
+                    {
+                        Turn(-90);
+                        TurnBike("left");
+                        StartCoroutine(HideForRayCast(hit.collider));
+                    }
+                    else Move();
+                    return;
+
+                default: return;
+            }
+        }
+        Debug.DrawRay(transform.position, transform.right, Color.green);
+    }
+    IEnumerator HideForRayCast(Collider me)
+    {
+        me.enabled = false;
+        yield return new WaitForSeconds(1);
+        me.enabled = true;
+    }
+    void Move()
+    {
+        transform.position = transform.position + GetAxis() * SpeedMove * Time.deltaTime;
+    }
+    void Turn(int Dir)
+    {
+        transform.Rotate(new Vector3(0, 0, transform.rotation.z + Dir), Space.Self);
+    }
+    Vector3 GetAxis()
+    {
+        switch (VectorMove)
+        {
+            case vectorMove.Up: return Vector3.forward;
+            case vectorMove.Right: return Vector3.right;
+            case vectorMove.Left: return Vector3.left;
+            case vectorMove.Down: return -Vector3.forward;
+        }
+        return new Vector3();
     }
 
-    // Update is called once per frame
-    void Update()
+    void TurnBike(string turnDir)
     {
-        
+        switch (VectorMove)
+        {
+            case vectorMove.Up:
+                if (turnDir == "left") VectorMove = vectorMove.Left;
+                else if (turnDir == "right") VectorMove = vectorMove.Right;
+                return;
+            case vectorMove.Right:
+                if (turnDir == "left") VectorMove = vectorMove.Up;
+                else if (turnDir == "right") VectorMove = vectorMove.Down;
+                return;
+            case vectorMove.Left:
+                if (turnDir == "left") VectorMove = vectorMove.Down;
+                else if (turnDir == "right") VectorMove = vectorMove.Up;
+                return;
+            case vectorMove.Down:
+                if (turnDir == "left") VectorMove = vectorMove.Right;
+                else if (turnDir == "right") VectorMove = vectorMove.Left;
+                return;
+        }
     }
 }
---
+++ Assets/Scritps/CrossroadsController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossroadsController : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float Interval;
+    [SerializeField] private typeCrossroadsController TypeCrossroadsController;
+
+    [Header("Resources")]
+    [SerializeField] private Collider[] Stop;
+    [SerializeField] private Renderer[] LightYellow;
+    [SerializeField] private Renderer[] LightGreen;
+    [SerializeField] private Renderer[] LightRad;
+    [SerializeField] private Material[] MaterialLightOn;
+    [SerializeField] private Material[] MaterialLightOff;
+    private int temp;
+    enum typeCrossroadsController
+    {
+        OneSide,
+        TwoSides,
+        AllToAll
+    }
+    void Start()
+    {
+        switch (TypeCrossroadsController)
+        {
+            case typeCrossroadsController.OneSide:
+                temp = Random.Range(0, Stop.Length);
+                StartCoroutine(MainCtrl());
+                return;
+            case typeCrossroadsController.TwoSides:
+                // code
+                return;
+            case typeCrossroadsController.AllToAll:
+                // code
+                return;
+
+            default: return;
+        }
+    }
+    #region OneSide Crossroads Controller
+    IEnumerator MainCtrl()
+    {
+        while (true)
+        {
+            StartCoroutine(LightsOn(temp));
+            yield return new WaitForSeconds(Interval);
+            LightsOff(temp);
+            if (temp < Stop.Length - 1) temp++;
+            else temp = 0;
+        }
+    }
+    void LightsOff(int _temp)
+    {
+        Stop[_temp].enabled = true;
+        LightRad[_temp].material = MaterialLightOn[2];
+        LightYellow[_temp].material = MaterialLightOff[1];
+        LightGreen[_temp].material = MaterialLightOff[0];
+    }
+    IEnumerator LightsOn(int _temp)
+    {
+        Stop[_temp].enabled = false;
+        LightRad[_temp].material = MaterialLightOff[2];
+        LightYellow[_temp].material = MaterialLightOn[1];
+        yield return new WaitForSeconds(1);
+        LightYellow[_temp].material = MaterialLightOff[1];
+        LightGreen[_temp].material = MaterialLightOn[0];
+    }
+    #endregion
+
+    #region TwoSides Crossroads Controller
+
+    #endregion
+
+    #region AllToAll Crossroads Controller
+
+    #endregion
+}
